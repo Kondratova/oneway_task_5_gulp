3ab41056f4f2b4cd2cb1ff062f5582f61c27053e
Svetlana Kondratova <niimuratomai@rambler.ru>
2019-07-11 19:17:53 +0300

    v 0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 let { watch, src, dest, parallel, series  } = require('gulp');
 let browserSync = require('browser-sync');
-// let del = require('del');
+let del = require('del');
 let imagemin = require('gulp-imagemin');
 let sass = require('gulp-sass');
 let postcss = require('gulp-postcss');
 let autoprefixer = require('autoprefixer');
 let cssnano = require('cssnano');
+let twig = require('gulp-twig');
 
 // Девсервер
 function devServer(cb) {
@@ -20,18 +21,25 @@ function devServer(cb) {
     cb();
 }
 
-/*function clearBuild() {
-    return del('build/');
-}*/
+function clearBuild() {
+    return del(['build/*.*',
+        'build/styles/*.*',
+        'build/scripts/*.*',
+        'build/assets/fonts/*.*',
+        'build/assets/img/*.*',
+        'build/assets/img/icons/*.*']);
+}
 
 // Сборка
 function buildPages() {
-    return src('src/pages/*.html')
+    // Пути можно передавать массивами
+    return src(['src/pages/*.twig', 'src/pages/*.html'])
+        .pipe(twig())
         .pipe(dest('build/'));
 }
 
 function buildStyles() {
-    return src('src/styles/*.sass')
+    return src('src/styles/*.scss')
         .pipe(sass())
         .pipe(postcss([
             autoprefixer(),
@@ -60,24 +68,23 @@ function buildAssets(cb) {
 }
 
 function watchFiles() {
-    watch('src/pages/*.html', buildPages);
-    watch('src/styles/*.sass', buildStyles);
+    watch(['src/pages/*.twig', 'src/pages/*.html'], buildPages);
+    watch('src/styles/*.scss', buildStyles);
     watch('src/scripts/**/*.js', buildScripts);
     watch('src/assets/**/*.*', buildAssets);
 }
 
 // Указываем функции, которые будут доступны из терминала
 // Команда «по умолчанию» -- default -- будет срабатывать при вызове gulp без аргументов
-// exports.default = series(
-//     parallel(buildPages, buildStyles, buildScripts, buildAssets),
-//     watchFiles
-// );
 
 exports.default =
-    parallel(
-        devServer,
-        series(
-            parallel(buildPages, buildStyles, buildScripts, buildAssets),
-            watchFiles
+    series(
+        clearBuild,
+        parallel(
+            devServer,
+            series(
+                parallel(buildPages, buildStyles, buildScripts, buildAssets),
+                watchFiles
+            )
         )
     );
