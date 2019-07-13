80eeceb4f4a3db0e9cfe02712748d418a59ce2cd
Svetlana Kondratova <niimuratomai@rambler.ru>
2019-07-14 00:28:09 +0300

    v 1.3

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,34 @@
-const img = document.querySelectorAll(".slider img");
-let curent = 0;
+(function () {
+    const imgs = document.querySelectorAll(".slider img");
+    let current = 0;
 
-//Изменяет значение css переменной отвечающей за значение opacity
-function slider (){
-    for (let i = 0; i < img.length; i++) {
-        img[i].style.setProperty("--hide-img", "0");
+    //Изменяет значение css переменной отвечающей за значение opacity
+    function slider(imgs, current) {
+        for (let i = 0; i < imgs.length; i++) {
+            imgs[i].style.setProperty("--hide-img", "0");
+        }
+        imgs[current].style.setProperty("--hide-img", "1");
     }
-    img[curent].style.setProperty("--hide-img", "1");
-}
 
-slider();
-//Обработчик события нажатия левой кнопки - показать предыдущую картинку
-document.querySelector(".buttonLeft").onclick = function () {
-    curent == 0 ? curent = img.length-1 : curent-- ;
-    slider();
-};
+    slider(imgs, current);
 
-//Обработчик события нажатия правой кнопки - показать слудующую картинку
-document.querySelector(".buttonRight").onclick = function () {
-    curent == img.length-1 ? curent = 0 : curent++ ;
-    slider();
-};
+    //Обработчик события нажатия левой кнопки - показать предыдущую картинку
+    document.querySelector(".buttonLeft").onclick = function () {
+        if (current === 0) {
+            current = imgs.length - 1;
+        } else {
+            current--;
+        }
+        slider(imgs, current);
+    };
+
+    //Обработчик события нажатия правой кнопки - показать слудующую картинку
+    document.querySelector(".buttonRight").onclick = function () {
+        if (current === imgs.length - 1) {
+            current = 0;
+        } else {
+            current++;
+        }
+        slider(imgs, current);
+    };
+})()
